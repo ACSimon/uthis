a0265187fccf6aae8684e747e152f64049c0bd02
Aaron Simon <aaroncsimon@gmail.com>
2013-07-20 09:24:40 -0500

    Initial commit -Untold History

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Uthis::Application.config.secret_token = '02b6e19f51d33012c4e129c1389bb3e549b99c728b383183c8ad7dc54c3198f2b4198ca93641c41fe7e6ec78fad51cd22bee6a7f5a8e0b5f4845c3984537bd48'
